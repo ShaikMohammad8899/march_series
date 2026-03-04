b86324e9626b53bf8487b95db6dcee8d051dac07
ShaikMohammad8899 <shaikmohammadyaseer@gmail.com>
2026-03-04 18:59:37 +0530

    Add files via upload

---
+++ runtime task.py
@@ -0,0 +1,23 @@
+'''name=str(input("Name:"))
+mob=int(input("Moblie no:"))
+mail=str(input("Mail-Id:"))
+clg=str(input("collage name:"))
+dept=str(input("branch:"))
+
+print("\n------STUDENT PROFILE-------")
+print("student name is:",name,"\nstudent mobile number is:",mob,"\nstudent mail :",mail,"\nstudent collage name:",clg,"\nstudent department :",dept)'''
+
+
+name=str(input("Name:"))
+mob=int(input("Moblie no:"))
+mail=str(input("Mail-Id:"))
+clg=str(input("collage name:"))
+dept=str(input("branch:"))
+
+print("\n------STUDENT PROFILE-------")
+print("student name is:",name)
+print("student mobile number is:",mob)
+print("student mail :",mail)
+print("student collage name:",clg)
+print("student department :",dept)  
+
