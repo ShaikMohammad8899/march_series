5b9557f7bebced89e96d0e0c5daaee57b2ae7868
ShaikMohammad8899 <shaikmohammadyaseer@gmail.com>
2026-03-05 16:38:11 +0530

    Add files via upload

---
+++ fee.py
@@ -0,0 +1,17 @@
+name=str(input("Name:"))
+course=int(input("total course fee:"))
+paid=int(input("paid amount:"))
+print("\n------FEE CALCULATOR-------")
+print("student name is:",name)
+print("total fee:",course)
+print("paid amount :",paid)
+print("due amount:",course-paid)
+
+'''
+course=int(input("course fee:"))
+hostal=int(input("hostal fee:"))
+print("\n------FEE CALCULATOR-------")
+print("course fee:",course)
+print("hostal fee:",hostal)
+print("total fee:",course+hostal)
+'''
