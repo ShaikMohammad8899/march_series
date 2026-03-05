746f2665bbdfe6bdc2d1448f960c19224f66e978
ShaikMohammad8899 <shaikmohammadyaseer@gmail.com>
2026-03-05 16:38:45 +0530

    Add files via upload

---
+++ if conditions.py
@@ -0,0 +1,103 @@
+#conditions in python
+
+#if condition using comparison operator
+'''a=2
+b=5
+if a<b:
+    print("true")
+'''
+'''
+a=2
+b=5
+if a>b:
+    print("true")
+'''
+'''a=20
+b=25
+if a<=b:
+    print("true")
+'''
+'''
+a=12
+b=15
+if a>=b:
+    print("false")
+'''
+'''a=17
+b=15
+if a>=b:
+    print("true")
+'''
+
+'''a=12
+b=15
+if a!=b:
+    print("not equal")'''
+
+'''
+a=50
+b=50
+if a==b:
+    print("equal")'''
+
+'''a=20
+b=15
+if a>=b:
+    print("a is greater")'''
+
+#run time
+'''a=int(input("A value:"))
+b=int(input("B value:"))
+if a<b:
+    print("lesser")'''
+
+'''a=int(input("A value:"))
+if a>20:
+    print("greater")'''
+
+'''a="python"
+if a=="python":
+    print("correct")'''
+
+'''a=input("Data:")
+if a=="python":
+    print("true")'''
+
+'''a="python"
+b="java"
+if a!=b:
+    print("true")'''
+
+a="python"
+b="python"
+if a==b:
+    print("true")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
