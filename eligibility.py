6dc7c69155cefa9d80230953abb06a2eda4aab15
ShaikMohammad8899 <shaikmohammadyaseer@gmail.com>
2026-03-08 22:34:31 +0530

    Add files via upload

---
+++ eligibility.py
@@ -0,0 +1,91 @@
+#eligibility checker
+'''a=int(input("enter tthe age:"))
+
+if a>=18:
+    print("you are eligible for vote")
+else:
+    print("you are not eligible")
+'''
+#even or odd
+'''a=int(input("enter the number:"))
+if a%2==0:
+    print("even")
+else:
+    print("odd")'''
+
+#vowels
+'''v=("a","e","i","o","u")
+b=input("enter alphabet:")
+
+if b in v:
+    print("it is an vowel")
+else:
+    print("it is a consonent")'''
+#leap year
+'''a=int(input("enter year:"))
+if a%4==0:
+    print("it is a leap year")
+else:
+    print("it is a not a leap year")'''
+
+#welcome guest
+'''a=str(input("enter name:"))
+if a=="yaseer":
+      print("welcome yaseer")
+else:
+    print("welcome guest")'''
+
+'''names=["mani","ram","likit","siva","yaseer"]
+a=str(input("enter name:"))
+if a in names:
+      print("welcome",a)
+else:
+    print("welcome guest")'''
+
+#social media login
+'''a="shaik_Yaseer"
+b="Yaseer8899"
+name=input("username:")
+password=input("password:")
+if name==a:
+    if password==b:
+        print("login successful")
+    else:
+        print("incorrect password")
+else:
+    print("inavlid credentials")'''
+
+
+'''name=input("username:")
+password=input("password:")
+if name=="shaik_Yaseer":
+    if password=="Yaseer8899":
+        print("login successful")
+else:
+    print("inavlid credentials")'''
+
+
+name=input("username:")
+password=input("password:")
+if name=="shaik_Yaseer" and password=="Yaseer8899":
+        print("login successful")
+else:
+    print("inavlid credentials")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
---
+++ nested if.py
@@ -0,0 +1,83 @@
+#Nested if
+
+'''a=9
+b=12
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+'''a=9
+b=12
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")
+    if a==b:
+        print("not equal")'''
+
+'''a=9
+b=12
+if a<b:
+    print("less")
+    if b<a:
+        print("greater")
+    elif a==b:
+        print("not equal")
+    else:
+        print("true")'''
+
+
+
+'''a=9
+b=12
+if a>b:
+    print("less")
+    if b>a:
+        print("greater")
+    if a!=b:
+        print("not equal")
+else:
+    print("false")'''
+
+'''a=9
+b=12
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")
+    else:
+        print("true")
+else:
+    print("false")'''
+
+
+a=9
+b=12
+if a<b:
+    print("less")
+    if b>a:
+        print("true")
+    elif a!=b:
+        print("not equal")
+    else:
+        print("false")
+else:
+    print(True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
